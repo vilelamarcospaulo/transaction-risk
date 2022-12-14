e7cedbe2a50b1c5db0099469061422562692ef85
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2022-12-14 15:21:41 -0300

    refact: make amountAbove rule generic for params

---
+++ internal/risk_evaluator/amount_above.go
@@ -0,0 +1,21 @@
+package riskevaluator
+
+import "github.com/vilelamarcospaulo/risk/internal/transaction"
+
+func AmountAbove5K() *evaluatorNode {
+	return amountAbove(5_000_00, Medium)
+}
+
+func amountAbove(valueInUsCents int, riskLevel RiskLevel) *evaluatorNode {
+	return &evaluatorNode{
+		next:      nil,
+		predicate: checkValueAbove(valueInUsCents),
+		riskLevel: riskLevel,
+	}
+
+}
+func checkValueAbove(valueInUsCents int) func(transaction transaction.Transaction, globalContext *[]transaction.Transaction) (bool, error) {
+	return func(transaction transaction.Transaction, globalContext *[]transaction.Transaction) (bool, error) {
+		return transaction.Amount_us_cents > valueInUsCents, nil
+	}
+}
